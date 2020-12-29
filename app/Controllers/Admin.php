1fa63456770757c562f7d1a550c3506aea0da583
samuelnelson1000 <samuelnelson1000@gmail.com>
2020-12-29 22:16:43 +0700

    edit admin/index.php,edit controller admin.php

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,10 +2,53 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\Models\KurirModel;
+use App\Models\PengirimanModel;
+use App\Models\Model;
 
 class Admin extends Controller{
 
+    protected $kurirModel;
+    protected $pengirimanModel;
+    protected $session;
+
+    public function __construct(){
+        $this->session = session();
+        $this->kurirModel = new KurirModel();
+        $this->pengirimanModel = new PengirimanModel();
+    }
+
     public function index(){
-        return view('admin/index');
+        $data['pengiriman'] = $this->pengirimanModel->getOpenedPengiriman();
+        echo view('admin/index', $data);
+    }
+    
+    public function take($idPengiriman){
+        $idKurir = $this->session->get('id');
+        $this->pengirimanModel->updateKurir($idPengiriman, $idKurir);
+        
+        return redirect()->to('/admin');
+    }
+
+    public function pengiriman_saya(){
+        $idKurir = $this->session->get('id');
+        $data['pengiriman'] = $this->pengirimanModel->getPengirimanKurir($idKurir);
+        return view('admin/admin-pengiriman', $data);
+    }
+    
+    public function change_state(){
+        $this->request->getmethod() === 'post';
+        $idPengiriman = $this->request->getVar('id_pengiriman');
+        $state = $this->request->getVar('status_pengiriman');
+        
+        $this->pengirimanModel->changeState($idPengiriman, $state);
+        
+        return redirect()->to('/admin/pengiriman_saya');
+    }
+    
+    public function data_diri(){
+        $idKurir = $this->session->get('id');
+        $data['dataDiri'] = $this->kurirModel->getById($idKurir);
+        return view('kurir/data-diri', $data);
     }
 }
